37e0ec28fb5ec52c2deca5e397a194259639a8a0
Johan <johan.peraldi@gmail.com>
2022-07-12 11:16:50 +0200

    Expand advanced search bars on click

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ const mainInputElement = document.getElementById('searchbar')
 const ingredientsInputElement = document.getElementById('ingredients')
 const appliancesInputElement = document.getElementById('appliances')
 const utensilsInputElement = document.getElementById('utensils')
+const advancedSearchInputElements = [
+  ingredientsInputElement,
+  appliancesInputElement,
+  utensilsInputElement
+]
 
 // Display main searchbar user input to the console if it has at least 3 characters
 mainInputElement.addEventListener('input', (event) => {
@@ -18,20 +23,44 @@ mainInputElement.addEventListener('input', (event) => {
 })
 
 // Open tags list
-ingredientsInputElement.addEventListener('click', (event) => {
-  const elementId = event.target.id
-  console.log(`${elementId} tag list should be opened`)
-})
+advancedSearchInputElements.forEach(element => element.addEventListener('click', (event) => {
+  const el = event.target
+  let categoryName;
+  switch (el.id) {
+    case 'ingredients':
+      categoryName = 'ingrédient'
+      break
+    case 'appliances':
+      categoryName = 'appareil'
+      break
+    case 'utensils':
+      categoryName = 'ustensile'
+  }
+  el.parentElement.style.width = '50%'
+  el.style.width = '100%'
+  el.placeholder = `Rechercher un ${categoryName}`
+  el.parentElement.lastElementChild.style.right = '3%'
+}))
 
-appliancesInputElement.addEventListener('click', (event) => {
-  const elementId = event.target.id
-  console.log(`${elementId} tag list should be opened`)
-})
-
-utensilsInputElement.addEventListener('click', (event) => {
-  const elementId = event.target.id
-  console.log(`${elementId} tag list should be opened`)
-})
+// Close tags list
+advancedSearchInputElements.forEach(element => element.addEventListener('blur', (event) => {
+  const el = event.target
+  let categoryName;
+  switch (el.id) {
+    case 'ingredients':
+      categoryName = 'Ingrédients'
+      break
+    case 'appliances':
+      categoryName = 'Appareils'
+      break
+    case 'utensils':
+      categoryName = 'Ustensiles'
+  }
+  el.parentElement.style.width = '17rem'
+  el.style.width = '17rem'
+  el.placeholder = `${categoryName}`
+  el.parentElement.lastElementChild.style.right = '10%'
+}))
 
 const init = () => {
   displayRecipes(recipes)
